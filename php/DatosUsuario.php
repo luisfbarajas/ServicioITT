8194cc03329b82a74fa36478d092177b751a9320
luisfbarajas <luisf.barajas.btz@gmail.com>
2018-03-03 21:48:47 -0800

    Despliegue de datos al inicio de sesion agregado, clase para consulta de datos de usuario creada

--- php/DatosUsuario.php
+++ php/DatosUsuario.php
@@ -1,13 +1,19 @@
 <?php
- include '../Conexion.php';
+class DatosUsuario{
+    public function Datos($nombre){
+        include ("../Conexion.php");
+        $Usuario= $nombre;
+        $Consulta="SELECT * FROM $alumno WHERE name ='$Usuario' ";
+        $resultado= mysqli_query($conexion,$Consulta) ;
+        $filas= $resultado->num_rows;
+            while($row = mysqli_fetch_array($resultado)){     
+                return $row["id"];
+            }
+            mysqli_close($conexion);
+     }
 
- function Datos($nombre){
-    $Usuario= $nombre;
-    $Consulta="SELECT * FROM $alumno WHERE Nombre= '$Usuario'";
-    
-
- }
-
-
-
-?>
+    }
+    //llamada a metodo de funcion ejemplo 
+    $funcion = new DatosUsuario;
+   echo $funcion-> Datos("luis");
+ ?>
