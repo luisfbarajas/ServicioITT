adfb1d67214c38e6a4d890d06c2a03fc2c377911
luisfbarajas <luisf.barajas.btz@gmail.com>
2018-09-30 22:54:43 -0700

    Cambio de tags Header y footer por elementos php

--- Proceso.php
+++ Proceso.php
@@ -1,44 +1,12 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Departamento de ingles.|Inicio</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/main.css">
-
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-
-
-
-   
-
-
-
-    </head>
-    <body style="background-image: url('img/fondo-itt.jpg')">
+<?php
+include_once 'php/headerHTML.php';
+?>
        
          <div class="header">
        <?php include 'menu.php'; ?>
-      </div><br/><br/>
-
-
-
-
-
-          
+      </div><br/><br/>       
         <b> <h2 class="Fecha-Titulo pt">Proceso para realizar tu examen de ingles.</h2></b>
-
-
 
   <section class="indicaciones">
     	
@@ -183,34 +151,8 @@
 <br>
 
 
-    <footer class="app-footer">
-            <div class="app-texto">
-                <h4>Instituto Nacional de Mexico</h4>
-                <h5>Instituto Tecnologico de Tijuana</h5>
-                <p>Calzada Del Tecnológico S/N, Fraccionamiento Tomas Aquino. Tijuana, Baja California.<br/> 
-                     C.P. 22414 Teléfono: +52 (664) 607 8400<br/></p>
-                     <img src="img/galgos.jpg">
-            </div>
-          
-        </footer>
+  
+  <?php 
+	include_once 'php/footerHTML.php';
 
-
-      
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-
-        <script src="js/vendor/bootstrap.min.js"></script>
-
-        <script src="js/main.js"></script>
-
-
-        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-    </body>
-</html>
+	?>
--- infoFecha.php
+++ infoFecha.php
@@ -59,26 +59,7 @@
            ?>
           </div>
     </div>
-<footer class="app-footer">
-  <div class="app-texto">
-   <h4>Instituto Nacional de Mexico</h4>
-   <h5>Instituto Tecnologico de Tijuana</h5>
-   <p>Calzada Del Tecnológico S/N, Fraccionamiento Tomas Aquino. Tijuana, Baja California.<br/> 
-      C.P. 22414 Teléfono: +52 (664) 607 8400<br/></p>
-   <img src="img/galgos.jpg" alt="galgo">
-  </div>
-</footer>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-        <script src="js/vendor/bootstrap.min.js"></script>
-        <script src="js/main.js"></script>
-      <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-        <script>
-            (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-            function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-            e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-            e.src='//www.google-analytics.com/analytics.js';
-            r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-            ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-        </script>
-    </body>
-</html>
+
+    <?php
+    include_once 'php/footerHTML.php';
+    ?>
