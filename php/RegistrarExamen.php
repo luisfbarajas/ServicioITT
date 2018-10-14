040fb4594260063a4aec4d4710f9defe289c8de7
luisfbarajas <luisf.barajas.btz@gmail.com>
2018-10-13 22:01:31 -0700

    Registro de datos en la DB fix

--- php/RegistrarExamen.php
+++ php/RegistrarExamen.php
@@ -1,21 +1,74 @@
 <?php
-require_once '../Conexion.php';
+class RegistroExamen
+{
+    private $Examen = null;
+    private $Pago = null;
+    private $id = null;
+    private $Folio = null;
+    private $conexion = null;
+    private $hora = null;
+    protected $Fechas = "fechas_examen";
+    private $idFecha = 0;
+    public function __construct($id, $folio, $Examen, $Pago, $conexion)
+    {
+        $this->id = $this->parseInt($id);
+        $this->Pago = substr($Pago, 0, 10);
+        $this->Examen = $Examen;
+        $this->Folio = $this->parseInt($folio);
+        $this->conexion = $conexion;
+    }
 
-$Id = $_POST['id'];
-$FolioP = $_POST['FolioP'];
-$FechaExamen = $_POST['FechaExamen'];
-$FechaPago = $_POST['FechaPago'];
+    private function getDate($fecha)
+    {
+        $result = date_create_from_format('d/m/y', $fecha);
+        return DateTime::createFromFormat('Y-m-d', $fecha)->format('Y-m-d');;
+    }
+    private function parseInt($number)
+    {
+        $result = (int)$number;
+        return $result;
+    }
+    private function getDateID($fecha)
+    {
+        $Query = "SELECT id, HORARIO FROM {$this->Fechas} WHERE FECHA = '{$fecha}' LIMIT 1";
+        $execute = mysqli_query($this->conexion, $Query);
+        $result = mysqli_fetch_assoc($execute);
+        return $result;
+    }
+    public function InsertData()
+    {
+        try {
+            $this->setDateData();
+            $Query = "INSERT INTO `r_examen` (`id`, `ID_FECHAS`, `ID_ALUMNO`, `HORA`, `FechaPago`, `FolioPago`) VALUES (NULL, {$this->idFecha}, $this->id, '{$this->hora}', '{$this->Pago}', {$this->Folio});";
+            $execute = mysqli_query($this->conexion, $Query);
+            mysqli_close($this->conexion);
+        } catch (Exception $e) {
+            echo $e->getMessage();
+        }
+        return $execute;
+    }
+    private function setDateData()
+    {
+        $datos = $this->getDateID($this->Examen);
+        $this->idFecha = $datos['id'];
+        $this->hora = $datos['HORARIO'];
 
-
-
-$Query = "INSERT INTO $RegistroExamen (ID_FECHAS,ID_ALUMNO,HORA,FechaPago,FolioPago) VALUES (11,$Id,'8:00AM','$FechaPago','$FolioP')";
-
-$execute = mysqli_query($conexion, $Query);
-
-if(!$execute){
-   return trigger_error(mysqli_error($conexion));
+    }
 }
 
-mysqli_close($conexion);
+if (isset($_POST)) {
+    $Id = $_POST['id'];
+    $FolioP = $_POST['FolioP'];
+    $FechaExamen = $_POST['FechaExamen'];
+    $FechaPago = $_POST['FechaPago'];
+    // $Id = 14;
+    // $FolioP = 123545698;
+    // $FechaExamen = '2018-12-31';
+    // $FechaPago = '2018-12-31';
 
-echo $execute;
+    include_once '../Conexion.php';
+    $registro = new RegistroExamen($Id, $FolioP, $FechaExamen, $FechaPago, $conexion);
+
+    $excecute = $registro->InsertData();
+    echo $excecute;
+}
